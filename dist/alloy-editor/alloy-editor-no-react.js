0cf91f31923c1db9368b5886db4b65c0e8ebeb16
Anastasia Zotova <azotova@gmail.com>
2016-01-28 12:40:36 +0000

    Added example, theorem and NB buttons

--- dist/alloy-editor/alloy-editor-no-react.js
+++ dist/alloy-editor/alloy-editor-no-react.js
@@ -6184,7 +6184,7 @@ function _typeof(obj) { return obj && typeof Symbol !== "undefined" && obj.const
                     add: {
                         buttons: {
                           original: ['image', 'camera', 'hline', 'table'], // original Alloyeditor configuration
-                          section: ['Chart', 'Quiz', 'image', 'hline', 'table', 'Def'],
+                          section: ['Chart', 'Quiz', 'image', 'hline', 'table', 'Def', 'Example', 'Theorem', 'Nb'],
                           condition: ['Question', 'Choice', 'Construct', 'Chart', 'image', 'table'],
                           complexObject: ['Paragraph'], // for table, construct, chart, formula
                           defaultOption: ['image', 'camera', 'hline', 'table']
